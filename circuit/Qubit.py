02fd4694342ca77faa21e4b1310f91098b06da01
Isidore Carinae <eag4013@gmail.com>
2019-08-12 10:38:05 +0530

    Fixed Norm errors and amplitudes can be now complex

--- circuit/Qubit.py
+++ circuit/Qubit.py
@@ -5,18 +5,25 @@ Created on Sun Aug 11 09:58:27 2019
 @author: Eesh Gupta
 """
 class Qubit(object):
-    def __init__(self, i, a=1, b=0):
+   
+    def __init__(self, i =None, a=1, b=0):
         """
-        Qubit has 2 complex amplitudes for 2 basis states, 0 and 1
-        Initialized at basis state 0.
-        Checks normalization condition for qubit basis state amplitudes.
+        Input: 2 complex amplitudes, a and b, for 2 basis states, 0 and 1
+               int i to refer to as id. Used for identification for register.
+        
         """
-        self._i = i
         self._a = a
         self._b = b
+        self._id=i
         
     
-    def _normalize(a, b): 
+    def _normalize(self, a, b): 
+        """
+        Input: int a and b 
+        Output: Checks if amplitudes are normalized.
+        """
+        a=int(a)
+        b=int(b)
         if (a**2 +b**2) !=1: 
             raise ValueError("Amplitude values must be normalized!")
         
@@ -26,14 +33,22 @@ class Qubit(object):
     
     @a.setter
     def a(self, value): 
-        self._normalize(a = value, b = self._b)
-        self._a=value
-        
+        if abs(value)<=1: 
+            self._a=value
+            self._b=math.sqrt(1-((abs(value))**2))
+        else: 
+            raise ValueError("Since Amplitude values must be normalized," + 
+                             " norm of a must be less than or equal to 1")
+            
     @property
     def b(self): 
         return self._b
     
     @b.setter
     def b(self, value): 
-        self._normalize(a = self._a, b = value)
-        self._b=value
+        if abs(value)<=1: 
+            self._b=value
+            self._a=sqrt(1-((abs(value))**2))
+        else: 
+            raise ValueError("Since Amplitude values must be normalized," + 
+                             " norm of b must be less than or equal to 1")
